28e3237a12c490535d32cca63821e34b37736c89
Rafid Tahmid <rafidtahmid@Rafids-MacBook-Air.local>
2025-09-30 14:57:22 +0930

    Fix database connection error handling

--- Main_Project/db.js
+++ Main_Project/db.js
@@ -13,10 +13,14 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
   if (err) {
     console.error('Database connection failed:', err.message);
-    console.log('Make sure MySQL is running and the database is set up correctly.');
-    throw err;
+    console.error('DB_HOST:', process.env.DB_HOST);
+    console.error('DB_USER:', process.env.DB_USER);
+    console.error('DB_NAME:', process.env.DB_NAME);
+    console.error('Make sure environment variables are set in Railway and MySQL service is running.');
+    // Don't throw - let the app start so we can see error pages
+  } else {
+    console.log('Connected to MySQL database');
   }
-  console.log('Connected to MySQL database');
 });
 
 module.exports = connection;
